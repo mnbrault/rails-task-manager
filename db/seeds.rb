377aac1208facd8e038f094314a4ebe4e81a3684
Martin Demoustier-Brault <mn.brault@gmail.com>
2020-05-12 16:33:30 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  new_task = Task.new(title: Faker::Hipster.word, details: Faker::Hipster.sentences)
+  new_task.save
+end
+
+
